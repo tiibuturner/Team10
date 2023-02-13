119e26436befee38378d885db43487cca48fe7b7
Linzku <linnea.laukkanen@dnainternet.net>
2023-02-13 14:21:38 +0200

    kuvagallerian alku

---
+++ kuvagalleria/dbConfig.php
@@ -0,0 +1,14 @@
+<?php  
+// Database configuration  
+$dbHost     = "db";  
+$dbUsername = "root";  
+$dbPassword = "password";  
+$dbName     = "kuvagalleria";  
+  
+// Create database connection  
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);  
+  
+// Check connection  
+if ($db->connect_error) {  
+    die("Connection failed: " . $db->connect_error);  
+}
