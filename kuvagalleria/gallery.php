cdcb55b950566223f395184579019bca98cb1cd0
Linzku <linnea.laukkanen@dnainternet.net>
2023-02-17 14:51:51 +0200

    joitain

--- kuvagalleria/gallery.php
+++ kuvagalleria/gallery.php
@@ -37,7 +37,7 @@
         <main>
     
         <section class="gallery-links">
-        <h1 class="h1">Kansallispuiston kuvagalleria</h1>
+        <h1 class="h1">Kansallispuiston kuvagalleria</h1><br><br>
             <div class="wrapper">
                 
                 <div class="gallery-container">
@@ -75,23 +75,25 @@
                 </div>
         </section>
         <section><div>
-                   
+        <p>Haluatko lisätä omia kansallispuisto muistojasi kuvagalleriaamme?</p><br>
+        <p>Pääset <a href="uploadlomake.php" ><input type="button" name="Täältä" value="Täältä" class="pekka" ></a> lisäämään omia kuviasi galleriaan.</p>
+        <br><br>           
 <?php
         
-               echo ' <div class="gallery-upload">
-                    <form action="gallery-upload.inc.php" method="post" enctype="multipart/form-data">
-                    <input type="text" name="filename" placeholder="Tiedoston nimi..."><br><br>
-                    <input type="text" name="filetitle" placeholder="Kuvan Otsikko..."><br><br>
-                    <input type="text" name="filedesc" placeholder="Kerro kuvastasi..."><br><br>
-                    <input type="file" name="file">
-                    <button type="submit" name="submit">Upload</button>
+            //    echo ' <div class="gallery-upload">
+            //         <form action="gallery-upload.inc.php" method="post" enctype="multipart/form-data">
+            //         <input type="text" name="filename" placeholder="Tiedoston nimi..."><br><br>
+            //         <input type="text" name="filetitle" placeholder="Kuvan Otsikko..."><br><br>
+            //         <input type="text" name="filedesc" placeholder="Kerro kuvastasi..."><br><br>
+            //         <input type="file" name="file">
+            //         <button type="submit" name="submit">Upload</button>
                     
 
 
-                    </form>
+            //         </form>
 
 
-                </div>';
+            //     </div>';
 ?>
             
                 </div>
@@ -106,6 +108,14 @@
         </div>
     </footer>
 
+
+    <script>
+    Mene(){
+         
+        window.open(uploadlomake.php);
+        
+        }
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
     
--- kuvagalleria/uploadlomake.php
+++ kuvagalleria/uploadlomake.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/general.css">
     <link rel="stylesheet" href="../assets/css/linneastyle.css">
-    <link rel="stylesheet" href="stylegallery.css">
+    
 </head>
 <body>
 <header class="image row"><a href="index.html"></a>
