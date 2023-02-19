8cddbacc5ff704ddfcc1243705470dd916011a4a
Linzku <linnea.laukkanen@dnainternet.net>
2023-02-19 19:46:22 +0200

    pieni siistiminen ja kommentointi uploadlomakkeese

--- kuvagalleria/uploadlomake.php
+++ kuvagalleria/uploadlomake.php
@@ -11,58 +11,80 @@
     
 </head>
 <body>
-<header class="image row"><a href="index.html"></a>
-    <h1 class="headerfp">Kanta-Hämeen Kansallispuistot</h1>
+    
+    <!-- header osuus -->
+    <header class="image row"><a href="index.html"></a>
+
+        <h1 class="headerfp">Kanta-Hämeen Kansallispuistot</h1>
+
     </header>
 
     <main>
         
-            <div class="main">
-                
-                <nav class="nav flex-column">
-                    <ul class="margin">
-                        <li><a href="../index.html">Etusivu</a></li>
-                        <li><a href="../page1.html">Torronsuo</a></li>
-                        <li><a href="../page2.html">Liesjärvi</a></li>
-                        <li><a href="../page3.html">Yleistä</a></li>
-                        <li><a href="../page4.php">Vieraskirja</a></li>
-                        <li><a href="gallery.php">Kuvagalleria</a></li>
-                    </ul>
-                </nav>
-                <section>
-                <h1 class="h1">Kansallispuiston kuvagalleria</h1>
+        <div class="main">
+             
+            <!-- navbarin laitto -->
+            <nav class="nav flex-column">
+
+                <ul class="margin">
+                    <li><a href="../index.html">Etusivu</a></li>
+                    <li><a href="../page1.html">Torronsuo</a></li>
+                    <li><a href="../page2.html">Liesjärvi</a></li>
+                    <li><a href="../page3.html">Yleistä</a></li>
+                    <li><a href="../page4.php">Vieraskirja</a></li>
+                    <li><a href="gallery.php">Kuvagalleria</a></li>
+                </ul>
+
+            </nav>
+
+        <section>
+
+            <h1 class="h1">Kuvien lataus kansallispuiston kuvagalleriaan</h1>
+
                 <div class="gallery-container">
                     
-    <?php
+                <?php
 
-        echo ' <div class="gallery-upload">
-        <form action="gallery-upload.inc.php" method="post" enctype="multipart/form-data">
-            <input type="text" name="filename" placeholder="Tiedoston nimi..."><br><br>
-            <input type="text" name="filetitle" placeholder="Kuvan Otsikko..."><br><br>
-            <input type="text" name="filedesc" placeholder="Kerro kuvastasi..."><br><br>
-            <input type="file" name="file">
-            <button type="submit" name="submit">Upload</button>
-        </form></div>';
+                    // kuvien upload lomakkeen toteutus.
+                    echo ' <div class="gallery-upload">
+                        <form action="gallery-upload.inc.php" method="post" enctype="multipart/form-data">
+
+                            <input type="text" name="filename" placeholder="Tiedoston nimi..."><br><br>
+                            <input type="text" name="filetitle" placeholder="Kuvan Otsikko..."><br><br>
+                            <input type="text" name="filedesc" placeholder="Kerro kuvastasi..."><br><br>
+                            <input type="file" name="file">
+                            <button type="submit" name="submit">Upload</button>
+
+                        </form>
+                
+                    </div>';
 
 
 
 
-    ?>
-     </div>
-            </section>
+                ?>
 
-        </main>
-    </div>
-    <footer class="row">
-        <div style="clear: both;">
-        <p class="eka">Tiina, Marika & Linnea @ 2023</p>
-        <p class="toka">Kuvat © Marika Salonen</p>
+                </div>
+        
+        </section>
+
         </div>
+    
+    </main>
+
+    <!-- footer osa -->
+    <footer class="row">
+
+        <div style="clear: both;">
+
+            <p class="eka">Tiina, Marika & Linnea @ 2023</p>
+            <p class="toka">Kuvat © Marika Salonen</p>
+
+        </div>
+
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
-    
-    
+ 
 </body>
 </html>
