647b782686338f212eb367c921c442344fadc26c
Linzku <linnea.laukkanen@dnainternet.net>
2023-02-20 16:02:40 +0200

    pieniä juttuja lisäyksiä

--- kirjaudu.php
+++ kirjaudu.php
@@ -10,8 +10,8 @@ print "<h2>Kirjaudu</h2>";
 ?>
 
 <form action="tarkistakirjautuminen.php" method="post">
-Tunnus:<br> <input type="text" name="tunnus" value="" size="25"><br><br>
-Salasana:<br> <input type="password" name="salasana" value="" size="25"><br><br>
+Tunnus:<br> <input type="text" name="tunnus" value="" size="25" required oninvalid="this.setCustomValidity('Et kirjoittanut tunnustasi. Tunnuksesi tähän.')"><br><br>
+Salasana:<br> <input type="password" name="salasana" value="" size="25" required oninvalid="this.setCustomValidity('Et kirjoittanut salasanaasi. Kirjoita salasanasi tähän')"><br><br>
 <input type="submit" name="ok" value="OK"><br>
 </form>
 </body>
